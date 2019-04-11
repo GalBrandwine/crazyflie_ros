759d04324bff67c428758f598554c5398760fdb6
MargaritaLop <ritush77@gmail.com>
2019-04-11 23:04:52 +0300

    Inserting ROS commands into Grid class

---
+++ crazyflie_demo/scripts/Indoor/Agent.py
@@ -0,0 +1,232 @@
+import numpy as np
+import rospy
+from std_msgs.msg import String
+
+class Agent:
+
+    def __init__(self, AgentID, pos, res, x_lim, y_lim):
+        self.ID = AgentID
+        self.agent_alive = True
+        self.is_homing = False
+        self.velocityFactor = 50
+        self.step_noise_size = 20
+        self.step_snr = 1
+        self.stepSizeLimit = 30
+        self.step_factor = 1
+        self.next_pos = pos
+        self.current_pos = self.next_pos
+        self.next_heading = 0
+        self.current_heading = self.next_heading
+        self.VisibilityRange = 300
+        self.scanning_range = 200
+        self.repulse_range = self.VisibilityRange/10
+        self.pull_range = self.VisibilityRange*4/5
+        self.goal_orianted_flag_flip_prob = 0
+        self.goal_orianted_flag = True #np.random.rand(1) < self.goal_orianted_flag_flip_prob
+        self.reduced_neigbours_pos_list = list()
+        self.astar_path = []
+        self.x_lim = x_lim
+        self.y_lim = y_lim
+        self.res = res
+
+
+    def update_current_state(self, current_pos, current_heading):
+        self.current_pos = current_pos
+        self.current_heading = current_heading
+
+
+    def get_virtual_target_and_heading(self):
+        return self.next_pos, self.next_heading
+
+
+    def preform_step_sys_sim(self, current_pos, current_heading, neigbours_pos_list, matrix):
+        self.update_current_state(current_pos, current_heading)
+        self.reduced_neigbours_pos_list = self.neighborhood_reduction(neigbours_pos_list, matrix)
+        self.Dynam_Search_in_maze(self.reduced_neigbours_pos_list, matrix)
+        self.next_heading = np.random.rand() * np.pi / 4
+
+
+    def Dynam_Search_in_maze(self, NeighborsPosList, matrix):
+
+        max_count_val = 15
+        break_counter = 0
+        vec = np.zeros(2)
+        flag = False
+
+        try:
+            vec = [self.astar_path[0][0]-self.current_pos[0][0],self.astar_path[0][1]-self.current_pos[0][1]]
+        except:
+            vec = [self.next_pos[0][0] - self.current_pos[0][0], self.next_pos[0][1] - self.current_pos[0][1]]
+        while not flag and break_counter < max_count_val:
+            break_counter = break_counter + 1
+            step = self.step_noise_size * ([0.5, 0.5] - np.random.rand(2)) + vec
+            if self.is_step_legal(self.current_pos, step, matrix):
+                flag = True
+                if np.random.rand(1) < 0.8:
+                    for neighbor_pos in NeighborsPosList:
+                        if self.outOfLimit_Ando(neighbor_pos, step):
+                            flag = False
+                            break
+                        if not self.is_step_in_corridor(step, neighbor_pos, matrix):
+                            flag = False
+                            break
+                    else:
+                        break
+
+        if break_counter < max_count_val:
+            self.next_pos = self.current_pos + step
+            try:
+                del self.astar_path[0]
+            except:
+                pass
+
+
+# This is the important function, that should be rewriten
+    def Dynam_Search_in_maze_old(self, NeighborsPosList, matrix):
+        flag = False
+        break_counter = 0
+        rep_att_vec = np.zeros(2)
+
+        # Goal oriented movement
+        noise_fac = 1
+        if self.goal_orianted_flag:
+            optinal_goal_list = self.non_scaned_list(self.current_pos, 5 * self.scanning_range, matrix)
+            if optinal_goal_list.__len__() > 0:
+                vec = optinal_goal_list[0] - self.current_pos
+                goal_vec = vec / np.linalg.norm(vec)
+                rep_att_vec = rep_att_vec + goal_vec
+            else:
+                noise_fac = 5
+        else:
+            noise_fac = 2
+        # Neighbours oriented movement
+        for NeighborPos in NeighborsPosList:
+            #rep_att_vec = rep_att_vec - NeighborPos / np.linalg.norm(NeighborPos)
+            if np.linalg.norm(NeighborPos) < self.repulse_range:
+                rep_att_vec = rep_att_vec - NeighborPos / np.linalg.norm(NeighborPos) * self.VisibilityRange
+            if np.linalg.norm(NeighborPos) > self.pull_range*noise_fac:
+                rep_att_vec = rep_att_vec + NeighborPos / np.linalg.norm(NeighborPos) * self.VisibilityRange
+        if np.linalg.norm(rep_att_vec) > 0:
+            rep_att_vec = rep_att_vec/np.linalg.norm(rep_att_vec)
+        if np.random.rand() < self.goal_orianted_flag_flip_prob:
+            self.goal_orianted_flag = not self.goal_orianted_flag
+
+        if np.linalg.norm(rep_att_vec) > 0:
+            rep_att_vec = self.step_noise_size*self.step_snr*rep_att_vec/np.linalg.norm(rep_att_vec)
+
+
+        while not flag and break_counter < 20:
+            break_counter = break_counter + 1
+            step = self.step_noise_size * noise_fac * ([0.5, 0.5] - np.random.rand(2)) + rep_att_vec
+            if self.is_step_legal(self.current_pos, step, matrix):
+                flag = True
+                for neighbor_pos in NeighborsPosList:
+                    if self.outOfLimit_Ando(neighbor_pos, step):
+                        flag = False
+                        break
+                    if not self.is_step_in_corridor(step, neighbor_pos, matrix):
+                        flag = False
+                        break
+
+        if break_counter < 20:
+            self.next_pos = self.current_pos + step
+    # The "is_step_in_corridor" functions let connected agent to disconnect with small probability
+
+
+    def non_scaned_list(self, p, r, matrix):
+        non_sc_list = list()
+        base_unit_vector = [[0, 1]]
+        for theta in np.linspace(0, 2*np.pi, num=10, endpoint=False):
+            unit_vector = [[base_unit_vector[0][0]*np.cos(theta) - base_unit_vector[0][1]*np.sin(theta),
+                            base_unit_vector[0][0] * np.sin(theta) + base_unit_vector[0][1] * np.cos(theta)]]
+            for dr in np.linspace(self.res, r, num=r / self.res * 2, endpoint=True):
+                p2 = p + [[unit_vector[0][0]*dr, unit_vector[0][1]*dr]]
+                i, j = self.xy_to_ij(p2[0][0], p2[0][1])
+                if 0 <= i and i < matrix.shape[0] and 0 <= j and j < matrix.shape[1]:
+                    if matrix[i][j] == 2:
+                        break
+                    else:
+                        if matrix[i][j] == 0:
+                            non_sc_list.append([self.ij_to_xy(i, j)])
+        return non_sc_list
+
+
+    def is_step_in_corridor(self, step, neighbor_pos, matrix):
+        neighbor_abs_pos = self.current_pos + neighbor_pos
+        if self.is_step_legal(neighbor_abs_pos, step, matrix):
+            neighbor_abs_pos_potential = neighbor_abs_pos + step
+        else:
+            neighbor_pos_unit = neighbor_pos / np.linalg.norm(neighbor_pos)
+            neighbor_step_potential = step - 2 * np.dot(step[0], neighbor_pos_unit[0]) * neighbor_pos_unit
+            neighbor_abs_pos_potential = neighbor_abs_pos + neighbor_step_potential
+
+        return self.is_los(self.current_pos + step, neighbor_abs_pos_potential, matrix)
+
+
+    def outOfLimit_Ando(self, neighbor_pos, step):
+        avg_pos = np.divide(neighbor_pos, 2)
+        deltas_step = step - avg_pos
+        return np.linalg.norm(deltas_step) > self.VisibilityRange/2
+
+
+    def Sensing(self, agents_arr, matrix):
+        neighbors_pos = []
+        for i in range(0, agents_arr.__len__()):
+            diff = agents_arr[i].current_pos - self.current_pos
+            if (agents_arr[i].ID != self.ID and np.linalg.norm(diff) < self.VisibilityRange and
+                    self.is_los(agents_arr[i].current_pos, self.current_pos, matrix)):
+                neighbors_pos.append(diff)
+        return neighbors_pos
+
+
+    def neighborhood_reduction(self, neighbors_pos, matrix):
+        reduced_neighbors_pos = []
+        for i in range(0, neighbors_pos.__len__()):
+            flag = True
+            counter = 0
+            for j in range(0, neighbors_pos.__len__()):
+                if i != j and ((np.linalg.norm(neighbors_pos[i]) > np.linalg.norm(neighbors_pos[j])) and
+                               (np.linalg.norm(neighbors_pos[i]) >
+                                np.linalg.norm(neighbors_pos[j] - neighbors_pos[i]))):
+                    if self.is_los(self.current_pos + neighbors_pos[i], self.current_pos + neighbors_pos[j], matrix):
+                        counter = counter + 1
+                        if counter > 0:
+                            flag = False
+                            break
+            if flag:
+                reduced_neighbors_pos.append(neighbors_pos[i])
+        return reduced_neighbors_pos
+
+
+    def update_sate(self, pos, heading):
+        self.current_pos = pos
+        self.current_heading = heading
+
+
+    def is_step_legal(self, curr_pos, step, matrix):
+        new_pos = curr_pos + step
+        i, j = self.xy_to_ij(new_pos[0][0], new_pos[0][1])
+        if not (0 <= i and i < matrix.shape[0] and 0 <= j and j < matrix.shape[1] and (matrix[i][j] == 1 or matrix[i][j] == 3)):
+            return False
+        return self.is_los(curr_pos, new_pos, matrix)
+
+
+    def xy_to_ij(self, x, y):
+        i = int(np.floor((x - self.x_lim[0])/self.res))
+        j = int(np.floor((y - self.y_lim[0]) / self.res))
+        return i, j
+
+    def ij_to_xy(self, i, j):
+        x = self.x_lim[0] + i*self.res + self.res/2
+        y = self.y_lim[0] + j*self.res + self.res/2
+        return x, y
+
+    def is_los(self, p1, p2, matrix):
+        n = int(np.maximum(1, np.ceil(np.linalg.norm(p1-p2)/self.res)*3))
+        x = np.linspace(p1[0][0], p2[0][0], num=n, endpoint=True)
+        y = np.linspace(p1[0][1], p2[0][1], num=n, endpoint=True)
+        for ind in range(1, n):
+            i, j = self.xy_to_ij(x[ind], y[ind])
+            if matrix[i][j] != 1 and matrix[i][j] != 3:
+                return False
+        return True
---
+++ crazyflie_demo/scripts/Indoor/Grid.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+import numpy as np
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import PointCloud2
+from crazyflie_driver.msg import GenericLogData
+import sensor_msgs.point_cloud2 as pc2
+
+class drone_pos:
+    def __init__(self, time = None, x = None, y = None, z = None, w = None):
+
+        self.time = time
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+
+
+class drone_pc:
+    def __init__(self, time=None, pc_sens=None):
+        self.time = time
+        self.pc_sens = pc_sens
+
+
+class Grid:
+
+    def __init__(self, border_polygon, res):
+        self.x_lim = [border_polygon[0][0], border_polygon[0][0]]
+        self.y_lim = [border_polygon[0][1], border_polygon[0][1]]
+        for i in range(1, border_polygon.__len__()):
+            if self.x_lim[0] > border_polygon[i][0]:
+                self.x_lim[0] = border_polygon[i][0]
+            if self.x_lim[1] < border_polygon[i][0]:
+                self.x_lim[1] = border_polygon[i][0]
+            if self.y_lim[0] > border_polygon[i][1]:
+                self.y_lim[0] = border_polygon[i][1]
+            if self.y_lim[1] < border_polygon[i][1]:
+                self.y_lim[1] = border_polygon[i][1]
+
+        self.res = res
+        self.matrix = np.zeros([np.int64(np.ceil((self.x_lim[1]-self.x_lim[0])/self.res)), np.int64(np.ceil((self.y_lim[1]-self.y_lim[0])/self.res))])
+        self.empty_idxs = []
+        self.wall_idxs = []
+        self.nDrones = 1
+        self.eps = 1 # allowed time difference between messages
+        self.m_to_cm = 100
+        self.min_rng = 20
+
+        self.drones_pos_list = dict()
+        self.drones_pc_list = dict()
+        for iDrone in range(self.nDrones):
+            # Init listeners
+            self.pc_sub = rospy.Subscriber("/cf4/point_cloud", PointCloud2,
+                                           self.point_cloud_parser)
+            self.pos_sub = rospy.Subscriber("/cf4/log_pos", GenericLogData,
+                                            self.pos_parser)
+
+
+    def point_cloud_parser(self, msg):
+        """Each publicitation, theres' an array of 10 points."""
+        point_cloud_last_timestamp = msg.header
+        point_cloud = pc2.read_points_list(msg, skip_nans=True)
+
+        sens = []
+        for i in range(len(point_cloud)):
+            point = point_cloud[i]
+            sens.append([point.x*self.m_to_cm, point.y*self.m_to_cm, point.z*self.m_to_cm])
+
+        drone_id = point_cloud_last_timestamp.frame_id
+        self.drones_pc_list[drone_id] = drone_pc(point_cloud_last_timestamp.stamp.secs, sens)
+
+        self.update_from_tof_sensing_list(drone_id)
+
+
+    def pos_parser(self, msg):
+        pos_header = msg.header
+        pos_val = msg.values
+        # rospy.loginfo(self.pos_header)
+        # rospy.loginfo(self.pos_val)
+
+        drone_id = pos_header.frame_id.split("/")[0]
+        self.drones_pos_list[drone_id] = drone_pos(pos_header.stamp.secs, pos_val[0]*self.m_to_cm, pos_val[1]*self.m_to_cm, pos_val[2]*self.m_to_cm, None)
+
+    def is_time_equal(self, t1, t2):
+        return abs(t1-t2) <= self.eps
+
+    def xy_to_ij(self, x, y):
+        i = int(np.floor((x - self.x_lim[0])/self.res))
+        j = int(np.floor((y - self.y_lim[0]) / self.res))
+        return i, j
+
+    def ij_to_xy(self, i, j):
+        x = self.x_lim[0] + i*self.res + self.res/2
+        y = self.y_lim[0] + j*self.res + self.res/2
+        return x, y
+
+    def change_tail_to_empty(self, i, j):
+        # self.change_tail_color_ij(i, j, 'k')
+        self.matrix[i][j] = 1
+
+    def change_tail_to_wall(self, i, j):
+        # self.change_tail_color_ij(i, j, 'w')
+        self.matrix[i][j] = 2
+
+    def update_from_tof_sensing_list(self, drone_id):
+        current_pos = self.drones_pos_list[drone_id]
+        current_pc = self.drones_pc_list[drone_id]
+        if not self.is_time_equal(current_pos.time, current_pc.time):
+            return
+
+        self.empty_idxs = []
+        self.wall_idxs = []
+
+        for elem in current_pc.pc_sens:
+            if np.linalg.norm([elem[0], elem[1]]) >= self.min_rng:
+                sensing_pos = [[current_pos.x+elem[0], current_pos.y+elem[1]]]
+                self.update_with_tof_sensor([[current_pos.x, current_pos.y]], sensing_pos, 1)
+
+    def update_with_tof_sensor(self, sensor_pos, tof_sensing_pos, is_tof_senses):
+        num_of_samples = int(np.floor(np.linalg.norm(tof_sensing_pos - sensor_pos) / self.res * 2))
+        xs = np.linspace(sensor_pos[0][0], tof_sensing_pos[0][0], num=num_of_samples, endpoint=True)
+        ys = np.linspace(sensor_pos[0][1], tof_sensing_pos[0][1], num=num_of_samples, endpoint=True)
+        for ind in range(1, num_of_samples):
+            i, j = self.xy_to_ij(xs[ind], ys[ind])
+            if 0 > i or i >= self.matrix.shape[0] or 0 > j or j >= self.matrix.shape[1]:
+                return
+            if self.matrix[i][j] == 0:
+                self.change_tail_to_empty(i, j)
+                self.empty_idxs.append([i,j])
+        if is_tof_senses:
+            d = tof_sensing_pos - sensor_pos
+            norm_d = np.linalg.norm(d)
+            if norm_d > 0:
+                wall_pos = tof_sensing_pos + d /norm_d*self.res/1000
+                i, j = self.xy_to_ij(wall_pos[0][0], wall_pos[0][1])
+                self.change_tail_to_wall(i, j)
+                self.wall_idxs.append([i, j])
+
+
+if __name__ == "__main__":
+
+    rospy.init_node("grid_builder")
+
+    grid = Grid([(0, 0), (570, 0), (570, 550), (0, 550), (0, 0)], 10)
+
+    rospy.spin()
